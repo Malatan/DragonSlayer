bfe21c85794990896012a7686768ab4c932ad0f7
Malatan <matteozchn124@gmail.com>
2020-10-05 21:13:12 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/DragonSlayer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DragonSlayer.dir/main.cpp.obj"
+  "DragonSlayer.exe"
+  "DragonSlayer.exe.manifest"
+  "DragonSlayer.pdb"
+  "libDragonSlayer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DragonSlayer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
