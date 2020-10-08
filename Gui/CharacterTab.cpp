6e39bc15bb9690c8d84cad10f14ae8e497747724
Malatan <matteozchn124@gmail.com>
2020-10-08 19:01:54 +0200

    aggiunto classe CharacaterTab per display dell inventario e le statistiche del player

---
+++ Gui/CharacterTab.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Zheng on 08/10/2020.
+//
+
+#include "CharacterTab.h"
---
+++ Gui/CharacterTab.h
@@ -0,0 +1,14 @@
+//
+// Created by Zheng on 08/10/2020.
+//
+
+#ifndef DRAGONSLAYER_CHARACTERTAB_H
+#define DRAGONSLAYER_CHARACTERTAB_H
+
+
+class CharacterTab {
+
+};
+
+
+#endif //DRAGONSLAYER_CHARACTERTAB_H
