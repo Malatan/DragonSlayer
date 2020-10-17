5302024b0582de9f54ef7ea4617201c9685bc506
Malatan <matteozchn124@gmail.com>
2020-10-17 03:49:55 +0200

    update gitignore e untrack files 2

--- unit_testing/cmake-build-release/lib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/DragonSlayer/unit_testing/lib
-# Build directory: F:/DragonSlayer/unit_testing/cmake-build-release/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
