a37e60392b3ab49d690bd486b8643742c02ae7d8
andrypon <andrypon@gmail.com>
2020-10-28 18:26:36 +0100

    BuildRelease

--- cmake-build-release/cmake_install.cmake
+++ cmake-build-release/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: F:/DragonSlayer
+# Install script for directory: C:/PROGETTO/MAIN/DragonSlayer
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("F:/DragonSlayer/cmake-build-release/unit_testing/cmake_install.cmake")
+  include("C:/PROGETTO/MAIN/DragonSlayer/cmake-build-release/unit_testing/cmake_install.cmake")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "F:/DragonSlayer/cmake-build-release/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/PROGETTO/MAIN/DragonSlayer/cmake-build-release/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- cmake-build-release/unit_testing/lib/googletest-master/CTestTestfile.cmake
+++ cmake-build-release/unit_testing/lib/googletest-master/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: F:/DragonSlayer/unit_testing/lib/googletest-master
-# Build directory: F:/DragonSlayer/cmake-build-release/unit_testing/lib/googletest-master
+# Source directory: C:/PROGETTO/MAIN/DragonSlayer/unit_testing/lib/googletest-master
+# Build directory: C:/PROGETTO/MAIN/DragonSlayer/cmake-build-release/unit_testing/lib/googletest-master
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
