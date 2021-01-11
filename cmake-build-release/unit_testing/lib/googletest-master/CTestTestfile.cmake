85bcf9533f8087baf85528fb8dd35aa7d635e1da
Malatan <40026469+Malatan@users.noreply.github.com>
2021-01-11 22:06:20 +0100

    Delete cmake-build-release directory

--- cmake-build-release/unit_testing/lib/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/Progetto/DragonSlayer/unit_testing/lib/googletest-master
-# Build directory: E:/Progetto/DragonSlayer/cmake-build-release/unit_testing/lib/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
