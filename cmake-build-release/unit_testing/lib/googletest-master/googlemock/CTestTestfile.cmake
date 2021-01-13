039e053519fa4b07176cd8f8368f5438c735f796
Malatan <matteozchn124@gmail.com>
2021-01-13 20:32:20 +0100

    fix

--- cmake-build-release/unit_testing/lib/googletest-master/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/Progetto/DragonSlayer/unit_testing/lib/googletest-master/googlemock
-# Build directory: E:/Progetto/DragonSlayer/cmake-build-release/unit_testing/lib/googletest-master/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
